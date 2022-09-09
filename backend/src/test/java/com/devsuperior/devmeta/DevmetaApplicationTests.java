6dcfad39d02917c21a47aa59048ea84c207909ab
Erik Johannes Steindorfer Proença <erik.proenca@orangedoor.com.br>
2022-09-08 22:43:42 -0300

    Project Created

---
+++ backend/src/test/java/com/devsuperior/devmeta/DevmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
