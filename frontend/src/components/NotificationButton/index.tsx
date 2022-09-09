35f5cb108188080300e87d2898b3bfe874617cc5
kirestein <erik.proenca2011@gmail.com>
2022-09-09 01:19:18 -0300

    First Component created

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import icon from "../../assets/img/notification-icon.svg";
+import "./styles.css"
 
 // import { Container } from './styles';
 
 const NotificationButton: React.FC = () => {
   return (
-    <div></div>
+    <div>
+      <div className="dsmeta-red-btn">
+        <img src={icon} alt="Notificar" />
+      </div>
+    </div>
   );
-}
+};
 
 export default NotificationButton;
