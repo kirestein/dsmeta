1343994234c20a9f7eddcd8fa479bcad25bb0ba0
kirestein <erik.proenca2011@gmail.com>
2022-09-09 01:04:32 -0300

    components folder created

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const NotificationButton: React.FC = () => {
+  return (
+    <div></div>
+  );
+}
+
+export default NotificationButton;
